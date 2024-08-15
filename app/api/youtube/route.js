2383588f766faf4d85c3f38835b2a8aa7e7a6f8d
ayyan akbar <raokhan627@gmail.com>
2024-08-15 16:55:38 +0500

    production issue checkingv1

--- app/api/youtube/route.js
+++ app/api/youtube/route.js
@@ -6,47 +6,50 @@ import { NextResponse } from "next/server";
 import { processAndStoreContent } from "@/utils/textProcessing";
 import { initPinecone } from "@/utils/pineconeUtils";
 
+
 export async function GET(req, res) {
   console.log("hitting");
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
+  async function fetchTranscriptWithRetry(url, retries = 3) {
+    for (let i = 0; i < retries; i++) {
+        try {
+            return await YoutubeTranscript.fetchTranscript(url);
+        } catch (error) {
+            console.warn(`Attempt ${i + 1} failed. Retrying...`);
+            if (i === retries - 1) throw error;
+        }
+    }
+}
   const url = new URL(req.url); // Replace with your base URL if needed
-
+  
   const searchParams = new URLSearchParams(url.search);
-
+ 
   const youtubelink = searchParams.get("youtubelink");
+  
 
   try {
-    console.log("Before yt transcription");
+    console.log("Before yt transcription")
 
+    
     // const transcript = await YoutubeTranscript.fetchTranscript(youtubelink);
-    try {
-      console.log("Fetching transcript for:", youtubelink);
-      const transcript = await YoutubeTranscript.fetchTranscript(youtubelink);
-      console.log("Transcript fetched successfully.");
-    } catch (fetchError) {
-      console.error("Error fetching transcript:", fetchError);
-      return new NextResponse(
-        "Error fetching transcript: " + fetchError.message,
-        {
-          status: 500,
-        }
-      );
-    }
+   
+      const transcript = await fetchTranscriptWithRetry(youtubelink);
+ 
 
-    console.log("after yt transcription: ", transcript);
-    console.log("Before all Text");
+    console.log("after yt transcription: ",transcript)
+    console.log("Before all Text")
     const allText = transcript.map((item) => item.text).join(" ");
-    console.log("after all Text: ", allText);
+    console.log("after all Text: ",allText)
 
     const pinecone = initPinecone();
     const index = pinecone.Index("ai-chat-bot"); // Replace with your Pinecone index name
-    console.log("pinecone index: ", index);
+    console.log("pinecone index: ",index);
     // Process and store the content
     await processAndStoreContent(index, allText);
-
+   
     return NextResponse.json({ status: 200 });
   } catch (error) {
     return new NextResponse("Error processing request: " + error.message, {
