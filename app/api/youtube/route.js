01673caa2e5102991a54d9c3a16707b1a92283b9
ayyan akbar <raokhan627@gmail.com>
2024-08-15 17:03:05 +0500

    Prod issue checking

--- app/api/youtube/route.js
+++ app/api/youtube/route.js
@@ -13,16 +13,7 @@ export async function GET(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  async function fetchTranscriptWithRetry(url, retries = 3) {
-    for (let i = 0; i < retries; i++) {
-        try {
-            return await YoutubeTranscript.fetchTranscript(url);
-        } catch (error) {
-            console.warn(`Attempt ${i + 1} failed. Retrying...`);
-            if (i === retries - 1) throw error;
-        }
-    }
-}
+
   const url = new URL(req.url); // Replace with your base URL if needed
   
   const searchParams = new URLSearchParams(url.search);
@@ -32,21 +23,15 @@ export async function GET(req, res) {
 
   try {
     console.log("Before yt transcription")
-
-    
-    // const transcript = await YoutubeTranscript.fetchTranscript(youtubelink);
-   
-      const transcript = await fetchTranscriptWithRetry(youtubelink);
- 
-
+    const transcript = await YoutubeTranscript.fetchTranscript(youtubelink);
     console.log("after yt transcription: ",transcript)
-    console.log("Before all Text")
+    
     const allText = transcript.map((item) => item.text).join(" ");
-    console.log("after all Text: ",allText)
+
 
     const pinecone = initPinecone();
     const index = pinecone.Index("ai-chat-bot"); // Replace with your Pinecone index name
-    console.log("pinecone index: ",index);
+  
     // Process and store the content
     await processAndStoreContent(index, allText);
    
